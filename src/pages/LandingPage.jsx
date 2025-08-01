bf1ecf63228933b4e5c90366629acc6559c87c4c
Alfred <alfredscyriac@gmail.com>
2025-07-31 22:40:07 -0400

    Feature: Update landing page hero section to be responsive

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,17 +10,20 @@ const LandingPage = () => {
             {/* Hero Section */}
             <section className='font-inter h-screen bg-cover bg-center bg-no-repeat absolute top-0 left-0 w-full' 
                 style={{backgroundImage: `url(${backgroundpic})`, filter: 'brightness(0.9)'}}>
-                    <div className='h-screen my-auto container mx-auto px-8 flex items-center justify-between'>
-                        <div className='flex-1 text-white space-y-10 mx-8'>
-                            <h1 className='font-semibold text-8xl'>We help you see <span className='italic'>beyond</span> the label</h1>
-                            <button className="font-inter text-1xl flex items-center justify-center space-x-2 px-5 py-2 bg-primarygreen hover:scale-105 rounded-lg transition-all duration-300 text-white cursor-pointer ">
+                    {/* Section Container */}
+                    <div className='min-h-screen container mx-auto px-8 flex flex-col md:flex-row items-center justify-start md:justify-between pt-24 md:pt-0 md:items-center'>
+                        {/* Text and Button Container */}
+                        <div className='w-full md:flex-1 text-white space-y-8 md:space-y-10 px-4 md:px-8 text-center md:text-left'>
+                            <h1 className='font-semibold text-5xl md:text-6xl lg:text-8xl'>We help you see <span className='italic'>beyond</span> the label</h1>
+                            <button className="font-inter text-2xl md:text-1xl flex items-center justify-center space-x-2 px-5 py-2 bg-secondarygreen md:bg-primarygreen hover:scale-105 rounded-lg transition-all duration-300 text-white cursor-pointer mx-auto md:mx-0">
                                 <div>Start Scanning</div>
                                 <ScanBarcode size={30} />
                             </button>
                         </div>
 
-                        <div className='flex-1 flex justify-center'>
-                            <img src={nutritionLabelImage} alt="Nutrition label example" className="drop-shadow-xl rounded-2xl rotate-2 max-w-lg"/>
+                        {/* Image Container */}
+                        <div className='w-full md:flex-1 flex justify-center mt-8 md:mt-0'>
+                            <img src={nutritionLabelImage} alt="Nutrition label example" className="drop-shadow-xl rounded-2xl md:rotate-2 w-90 lg:w-lg"/>
                         </div>
                     </div>
             </section>
