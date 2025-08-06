e38423368603d76ff18a2cade764a3b177ddc0ef
Alfred <alfredscyriac@gmail.com>
2025-08-06 19:22:19 -0400

    Debug: Fix navbar footer routing issues by importing useLocation from react router and splitting app function into two functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import backgroundpic from './assets/arogyambg.jpg' 
 import Navbar from './components/Navbar.jsx'
 import LandingPage from './pages/LandingPage'
@@ -9,21 +9,29 @@ import SignIn from './pages/SignIn'
 import ScrollToTop from './components/ScrollToTop'
 
 
-function App() {
+function AppContent() {
+  const location = useLocation()
   const isAuthPage = location.pathname === '/signin' || location.pathname === '/signup'
+  return (
+    <div className='font-inter min-h-screen w-full bg-cover bg-center bg-fixed bg-no-repeat absolute top-0 left-0' style={{backgroundImage: `url(${backgroundpic})`, filter: 'brightness(0.9)'}}>
+      {!isAuthPage && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/privacypolicy' element={<PrivacyPolicy />} />
+      </Routes>
+      {!isAuthPage && <Footer/>}
+    </div>
+  )
+}
+
+function App() {
   return (
     <Router>
       <ScrollToTop/>
-      <div className='font-inter min-h-screen w-full bg-cover bg-center bg-fixed bg-no-repeat absolute top-0 left-0' style={{backgroundImage: `url(${backgroundpic})`, filter: 'brightness(0.9)'}}>
-        {!isAuthPage && <Navbar/>}
-        <Routes>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path='/signin' element={<SignIn/>}/>
-          <Route path='/privacypolicy' element={<PrivacyPolicy />} />
-        </Routes>
-        {!isAuthPage && <Footer/>}
-      </div>
+      <AppContent/>
     </Router>
+
   )
 }
 
