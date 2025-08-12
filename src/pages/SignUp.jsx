fc0016cd0e5fac0fa05185728873d7047f555fe2
Alfred <alfredscyriac@gmail.com>
2025-08-12 11:13:55 -0400

    Chore: Re-size elements on sign up page to create more room for new section

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
+import logo from '../assets/arogyamlogo.png';
+import googlelogo from '../assets/Googlelogo.webp';
 import { useState } from 'react';
-import '../index.css'
-import logo from '../assets/arogyamlogo.png'
-import googlelogo from '../assets/Googlelogo.webp'
-import { Mail, Lock, Eye, EyeOff } from 'lucide-react'
-import { Link, useNavigate } from 'react-router-dom'
+import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -24,35 +24,48 @@ const SignUp = () => {
                 <img src={logo} alt="Arogyam Logo" className='h-11'/>
             </button>
 
-            <div className='w-full max-w-md space-y-4 mt-6'>
-                <div className='text-left space-y-4'>
-                    <h1 className='text-xl sm:text-2xl font-semibold'>Get started with Arogyam</h1>
-                    <p className='text-sm sm:text-base text-gray-300 font-light leading-relaxed'>
+            <div className='w-full max-w-md space-y-3 mt-8'>
+                <div className='text-left space-y-2'>
+                    <h1 className='text-lg sm:text-2xl font-semibold'>Get started with Arogyam</h1>
+                    <p className='text-sm text-gray-300 font-light leading-relaxed'>
                         You are one step closer to unlocking ingredient transparency.
                     </p>
                 </div>
 
-                <form className='space-y-6'>
-                    <div className='space-y-4'>
-                        <label className='block text-sm font-light'>Email</label>
+                <form className='space-y-4'>
+                    <div className='space-y-2'>
+                        <label className='block text-sm font-light'>Name</label>
                         <div className="relative">
-                            <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+                            <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
                             <input
-                                type="email"
-                                placeholder="Enter your email"
-                                className="w-full pl-10 pr-4 py-2 border border-gray-600 bg-black text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-lightgreen focus:border-transparent placeholder-gray-400"
+                                type="text"
+                                placeholder="Enter your first name"
+                                className="text-md w-full pl-10 pr-4 py-2 border border-gray-600 bg-black text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-lightgreen focus:border-transparent placeholder-gray-400"
                             />
                         </div>
                     </div>
 
-                    <div className='space-y-4'>
+
+                    <div className='space-y-2'>
+                        <label className='block text-sm font-light'>Email</label>
+                        <div className="relative">
+                            <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
+                            <input
+                                type="email"
+                                placeholder="Enter your email"
+                                className="text-md w-full pl-10 pr-4 py-2 border border-gray-600 bg-black text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-lightgreen focus:border-transparent placeholder-gray-400"
+                            />
+                        </div>
+                    </div>
+
+                    <div className='space-y-2'>
                         <label className='block text-sm font-light'>Password</label>
                         <div className="relative">
-                            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+                            <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
                             <input
                                 type={showPassword ? "text" : "password"}
                                 placeholder="Enter your password"
-                                className="w-full pl-10 pr-12 py-2 border border-gray-600 bg-black text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-lightgreen focus:border-transparent placeholder-gray-400"
+                                className="text-md w-full pl-10 pr-12 py-2 border border-gray-600 bg-black text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-lightgreen focus:border-transparent placeholder-gray-400"
                             />
                             <button
                                 type="button"
