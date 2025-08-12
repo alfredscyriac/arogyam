110d41e63188d901493d5dd86246730341d0f1ec
Alfred <alfredscyriac@gmail.com>
2025-08-12 13:13:34 -0400

    Feature: Dynamic password strength progression bar

--- src/components/PasswordStrengthMeter.jsx
+++ src/components/PasswordStrengthMeter.jsx
@@ -26,9 +26,52 @@ const PasswordCriteria = ({password}) => {
 }
 
 const PasswordStrengthMeter = ({password}) => {
-  return (
-    <div>PasswordStrengthMeter</div>
-  )
+    const getStrength = (pass) => {
+		let strength = 0;
+		if (pass.length >= 6) strength++;
+		if (pass.match(/[a-z]/) && pass.match(/[A-Z]/)) strength++;
+		if (pass.match(/\d/)) strength++;
+		if (pass.match(/[^a-zA-Z\d]/)) strength++;
+		return strength;
+	};
+
+    const strength = getStrength(password);
+
+	const getColor = (strength) => {
+		if (strength === 0) return "bg-red-500";
+		if (strength === 1) return "bg-red-400";
+		if (strength === 2) return "bg-yellow-500";
+		if (strength === 3) return "bg-yellow-400";
+		return "bg-green-500";
+	};
+
+	const getStrengthText = (strength) => {
+		if (strength === 0) return "Very Weak";
+		if (strength === 1) return "Weak";
+		if (strength === 2) return "Fairly Strong";
+		if (strength === 3) return "Very Strong";
+		return "Strong";
+	};
+    return (
+        <div className='mt-1'>
+            <div className='flex justify-center items-center mb-1'>
+                <span className='text-xs text-gray-300'>Password strength</span>
+                <span className='text-xs text-gray-300'>{getStrength(strength)}</span>
+            </div>
+            
+            <div className='flex space-x-1'>
+				{[...Array(4)].map((_, index) => (
+					<div
+						key={index}
+						className={`h-1 w-1/4 rounded-full transition-colors duration-300 
+                ${index < strength ? getColor(strength) : "bg-gray-600"}
+              `}
+					/>
+				))}
+			</div>
+			<PasswordCriteria password={password} />
+        </div>
+    )
 }
 
 export default PasswordStrengthMeter; 
